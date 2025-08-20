8566fc4246ae75f3ac13994c141d41bf6cccb497
Khalif <khalifaali12@gmail.com>
2025-08-20 19:08:09 -0400

    Add format

--- src/lib/colorize/colorize.h
+++ src/lib/colorize/colorize.h
@@ -7,12 +7,12 @@ extern char *YELLOW;
 extern char *WHITE;
 extern char *RED;
 extern char *RESET;
-//What we need to do is take in some input - 8 chracters
-//What should be our maximum input?
-//256 bytes take up 256 characters.
-// \u001b[32;1m
-//The escape sequence wouldn't be considered a printable character so 
-// I think we're ok
+// What we need to do is take in some input - 8 chracters
+// What should be our maximum input?
+// 256 bytes take up 256 characters.
+//  \u001b[32;1m
+// The escape sequence wouldn't be considered a printable character so
+//  I think we're ok
 
 void print_with_colors(FILE *output_stream, char *color, char *input);
 int valid_color(char *color);
