4ae9eadceafc9d89b39be885f1cda43628eeecdc
Khalif <khalifaali12@gmail.com>
2025-08-20 18:13:04 -0400

    Fix shit I broke??

--- tests/test_colorize.c
+++ tests/test_colorize.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 #include <string.h>
 #include <lib/colorize/colorize.h>
-#include <assert.h>
+#include <stdlib.h>
 
-void _assert_status(int expression, char *output) {
-    if (expression == 0) {
-        print_with_colors(GREEN, sprintf("%s passed.", output));
+void _assert_status(int expression, char *template_string) {
+    char formatted_output[256];
+    if (expression) {
+        sprintf(formatted_output, "%s passed.\n", template_string);
+        print_with_colors(stdout, GREEN, formatted_output);
     } else {
-        print_with_colors(RED, sprintf("%s failed.", output));
+        sprintf(formatted_output, "%s failed.\n", template_string);
+        print_with_colors(stderr, RED, formatted_output);
         exit(1);
     }
 }
@@ -17,11 +20,11 @@ int main() {
     //mapped to escape codes. We should only accept color constants
     //we've created.
 
-    _assert_status(valid_color("ORANGE") == 1, "Invalid color test");
-    _assert_status(valid_color("\033[2") == 1, "Invalid color test");
+    _assert_status(valid_color("ORANGE") == 0, "Invalid color test");
+    _assert_status(valid_color("\033[2") == 0, "Invalid color test");
     _assert_status(valid_color(BLACK) == 0, "Valid color test");
-    _assert_status(valid_color(RED) == 0, "Valid color test");
-    _assert_status(valid_color(WHITE) == 0, "Valid color test");
+    _assert_status(valid_color(RED) == 1, "Valid color test");
+    _assert_status(valid_color(WHITE) == 1, "Valid color test");
     confirm_colors(); //demo of print_with_colors.
 }
 
