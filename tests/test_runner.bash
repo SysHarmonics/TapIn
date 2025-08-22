a50ca718b35f2318e3537fe569667aeb7651d10f
Khalif <khalifaali12@gmail.com>
2025-08-22 09:57:20 -0400

    make a test runner script to simplify things

---
+++ tests/test_runner.bash
@@ -0,0 +1,21 @@
+#!/usr/bin/bash 
+
+# Ignore the src/main.c file
+linker_files=`find src -name "*.c" ! -name "main.c"`
+CFLAGS="-Wall -O2 -g -std=c11 -Isrc"
+LDFLAGS="-lsodium -lpthread"
+
+# Now loop through all the test file that have a main
+for test_file in `grep -l "main(" tests/test_*.c`; do
+  printf "............START..............\n"
+  printf "[+] Running %s\n" $test_file
+  gcc $test_file $linker_files tests/test_helpers.c $CFLAGS $LDFLAGS -o tmp_test_runner 
+  ./tmp_test_runner
+
+  test_status=$?
+
+  if [ $test_status -ne 0 ]; then
+    echo "Since test failed exiting..."
+  fi
+  printf "............END................\n\n"
+done
