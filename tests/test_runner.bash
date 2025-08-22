1248ba352cb5a6dae94809a9b9da4009ccb71b62
Khalif <khalifaali12@gmail.com>
2025-08-22 10:15:57 -0400

    Add support for sinlge test

--- tests/test_runner.bash
+++ tests/test_runner.bash
@@ -5,8 +5,17 @@ linker_files=`find src -name "*.c" ! -name "main.c"`
 CFLAGS="-Wall -O2 -g -std=c11 -Isrc"
 LDFLAGS="-lsodium -lpthread"
 
+# For this to work it needs to be a .c file
+single_test=$1
+
+test_files=`grep -l "main(" tests/test_*.c`
+
+if [[ "$single_test" != "" ]];then
+  test_files="tests/${single_test}"
+fi
+
 # Now loop through all the test files that have a main
-for test_file in `grep -l "main(" tests/test_*.c`; do
+for test_file in $test_files; do
   printf "............START..............\n"
   printf "[+] Running %s\n" $test_file
   gcc $test_file $linker_files tests/test_helpers.c $CFLAGS $LDFLAGS -o tmp_test_runner 
