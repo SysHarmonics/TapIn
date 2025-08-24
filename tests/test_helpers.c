cb4d1baaa21d45379c5ecd37a6b962731ee8808f
Khalif <khalifaali12@gmail.com>
2025-08-24 16:41:31 -0400

    add maxlen

--- tests/test_helpers.c
+++ tests/test_helpers.c
@@ -12,10 +12,10 @@ void assert_status(int expression, char *template_string) {
 
   char formatted_output[256];
   if (expression) {
-    snprintf(formatted_output, "%s passed.\n", template_string);
+    snprintf(formatted_output, 256, "%s passed.\n", template_string);
     print_with_colors(stdout, GREEN, formatted_output);
   } else {
-    snprintf(formatted_output, "%s failed.\n", template_string);
+    snprintf(formatted_output, 256, "%s failed.\n", template_string);
     print_with_colors(stderr, RED, formatted_output);
     exit(1);
   }
