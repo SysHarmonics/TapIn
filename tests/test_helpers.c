333373ea4b0c209cf35aa895792c91bf707bd915
Khalif <khalifaali12@gmail.com>
2025-08-24 16:39:32 -0400

    update sprintf to use a max buffer

--- tests/test_helpers.c
+++ tests/test_helpers.c
@@ -3,12 +3,19 @@
 #include <stdlib.h>
 
 void assert_status(int expression, char *template_string) {
+  int strlen = sizeof(template_string) / sizeof(template_string[0]);
+  // Account for null terminator (2 chars) and passed|failed (6)
+  if (strlen > 248) {
+    printf("Input too large. Consider shortening to a length <= 248 chars.;");
+    return;
+  }
+
   char formatted_output[256];
   if (expression) {
-    sprintf(formatted_output, "%s passed.\n", template_string);
+    snprintf(formatted_output, "%s passed.\n", template_string);
     print_with_colors(stdout, GREEN, formatted_output);
   } else {
-    sprintf(formatted_output, "%s failed.\n", template_string);
+    snprintf(formatted_output, "%s failed.\n", template_string);
     print_with_colors(stderr, RED, formatted_output);
     exit(1);
   }
